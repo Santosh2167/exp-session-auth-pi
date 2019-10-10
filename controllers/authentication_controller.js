69d1dc08b488b4cadb9c8d8559772f1924bc601d
karuna <santosh.poudyal@gmail.com>
2019-10-10 16:07:47 +1100

    completed registration form, mapped the form with the route, tested if it works, the route is /register

---
+++ controllers/authentication_controller.js
@@ -0,0 +1,25 @@
+function loginForm(req, res) {
+
+}
+
+function loginVerify(req, res) {
+
+}
+
+function make(req, res) {
+  res.render("auth/make");
+
+}
+
+function create(req, res) {
+
+}
+
+
+module.exports = {
+  loginForm,
+  loginVerify,
+  make,
+  create
+
+}
