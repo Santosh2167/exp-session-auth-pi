40146b7ee298f5a668babff6f4904590c00cb2a9
karuna <santosh.poudyal@gmail.com>
2019-10-10 15:21:35 +1100

    working towards authentication of the user, created schema to store user login information: email as username and password

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const PageController = require("./../controllers/page_controller");
+const AuthenticationController = require("./../controllers/authentication_controller");
 
 router.get("/", PageController.index);
 
